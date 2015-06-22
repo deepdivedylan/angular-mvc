50cda4a43f7c88820f33024f333075738f55a221
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-22 13:24:47 -0600

    Added standard PHP template files

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$CURRENT_DIR = __DIR__;
+$PAGE_TITLE = "Angular MVC Application";
+require_once("assets/php/header.php");
+?>
+	<div ng-view></div>
+<?php
+require_once("assets/php/footer.php");
