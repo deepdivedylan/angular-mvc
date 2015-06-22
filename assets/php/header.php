7cc11f07a676132f845d1c3742bc0f116a571b65
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-22 13:26:29 -0600

    Added blank CSS stylesheet

--- assets/php/header.php
+++ assets/php/header.php
@@ -17,7 +17,7 @@ $PREFIX = str_repeat("../", $DEPTH_DIFFERENCE);
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" />
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
-		<link type="text/css" href="<?php echo $PREFIX; ?>assets/css/abqtraffic.css" rel="stylesheet" />
+		<link type="text/css" href="<?php echo $PREFIX; ?>assets/css/angular-mvc.css" rel="stylesheet" />
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
