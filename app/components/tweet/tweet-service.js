a607b107ea2a0a91c62e70c3bc20e9dd0c35c744
Dylan McDonald <dmcdonald21@cnm.edu>
2015-06-22 11:12:52 -0600

    Created tweet REST service

---
+++ app/components/tweet/tweet-service.js
@@ -0,0 +1,30 @@
+app.constant("TWEET_ENDPOINT", "api/tweet/json/");
+app.service("TweetModel", function($http, TWEET_ENDPOINT) {
+	function getUrl() {
+		return(TWEET_ENDPOINT);
+	}
+
+	function getUrlForId(tweetId) {
+		return(getUrl() + tweetId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function(tweetId) {
+		return($http.get(getUrlForId(tweetId)));
+	};
+
+	this.create = function(tweet) {
+		return($http.post(getUrl(), tweet));
+	};
+
+	this.update = function(tweetId, tweet) {
+		return($http.post(getUrl(), tweet));
+	};
+
+	this.destroy = function(tweetId) {
+		return($http.delete(getUrlForId(tweetId)));
+	};
+});
